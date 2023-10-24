dd53f5add2accc596a2f87657d265cc7405260fa
Nicolas A. Gazaniga <121832571+Nicolas9213@users.noreply.github.com>
2023-10-24 19:59:46 -0300

    Add files via upload

---
+++ using-routes/App.js
@@ -0,0 +1,25 @@
+
+import HomeScreen from './src/HomeScreen';
+import UserData from './src/UserData';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="UserData" component={UserData} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
