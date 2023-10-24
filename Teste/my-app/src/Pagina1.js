fae237a46fc8a2e7a056bb4f424ad6e837e869de
Nicolas A. Gazaniga <nicolasgazaniga13@gmail.com>
2023-10-24 19:05:06 -0300

    24/10

---
+++ Teste/my-app/src/Pagina1.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Pagina1() {
+    return (
+        <View>
+            <Text>
+                Página 1
+            </Text>
+        </View>
+    )
+}
---
+++ Teste/my-app/src/Pagina2.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Pagina2() {
+    return (
+        <View>
+            <Text>
+                Página 2
+            </Text>
+        </View>
+    )
+}
